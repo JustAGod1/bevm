7fccf199d19ffa84a7631512e835911ff628abe0
JustAGod <hvalick2111@gmail.com>
2021-09-10 22:28:58 +0300

    Added macOS build script

---
+++ mac.sh
@@ -0,0 +1,8 @@
+cargo build --release
+install_name_tool -add_rpath @executable_path target/release/evm
+cp target/release/evm artifacts/BasePC\ 2.0.app/Contents/MacOS/evm
+cp target/release/libSDL2-2.0.dylib artifacts/BasePC\ 2.0.app/Contents/MacOS/libSDL2-2.0.dylib
+
+create-dmg --volname "BasePC 2.0" "artifacts/BasePC 2.0.app"
+
+mv "BasePC 2.0 0.01.dmg" "artifacts/BasePC 2.0.dmg"
--- windows.ps1
+++ windows.ps1
@@ -1,2 +1,2 @@
 cargo build --release
-Compress-Archive -Path "target/release/evm.exe", "target/release/SDL2.dll", "C:/Windows/System32/vcruntime140.dll" -DestinationPath "BasePC2_release.zip" -Update
+Compress-Archive -Path "target/release/evm.exe", "target/release/SDL2.dll", "C:/Windows/System32/vcruntime140.dll" -DestinationPath "artifacts/BasePC2_release.zip" -Update
